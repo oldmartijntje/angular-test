474a3a3cab3292c57a9e77981b3907b3bb77576e
oldmartijntje <oldmartijntje@gmail.com>
2022-05-11 15:27:24 +0200

    31:40

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,13 @@
+export const TASKS = [
+    {
+    id: 1,
+    text: 'Watching Anime',
+    day: 'May 29th at 1:30pm',
+    reminder: false,
+  },
+  {
+    id: 2,
+    text: 'Watching Agents of Shield',
+    day: 'May 28th at 1:30pm',
+    reminder: false,
+  }]
