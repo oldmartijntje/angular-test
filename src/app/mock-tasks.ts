3cee5e378485f03718165401ec641587176ac5f7
oldmartijntje <oldmartijntje@gmail.com>
2022-05-11 15:48:27 +0200

    34:28

--- src/app/mock-tasks.ts
+++ src/app/mock-tasks.ts
@@ -1,13 +1,22 @@
-export const TASKS = [
-    {
+import { Task } from './Task';
+
+export const TASKS: Task[] = [
+  {
     id: 1,
-    text: 'Watching Anime',
-    day: 'May 29th at 1:30pm',
-    reminder: false,
+    text: 'Doctors Appointment',
+    day: 'May 5th at 2:30pm',
+    reminder: true,
   },
   {
     id: 2,
-    text: 'Watching Agents of Shield',
-    day: 'May 28th at 1:30pm',
+    text: 'Meeting at School',
+    day: 'May 6th at 1:30pm',
+    reminder: true,
+  },
+  {
+    id: 3,
+    text: 'Food Shopping',
+    day: 'May 7th at 12:30pm',
     reminder: false,
-  }]
+  },
+];
